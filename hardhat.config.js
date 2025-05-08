a4b423f69062e1f8f2f33e005aeab3dfcc8fbbe8
Vasyl <kvasyl69@gmail.com>
2025-05-08 18:25:08 +0200

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+};
+
+
+/// package.json
+
+{
+  "name": "proofvault",
+  "version": "1.0.0",
+  "scripts": {
+    "test": "npx hardhat test"
+  },
+  "devDependencies": {
+    "@nomicfoundation/hardhat-toolbox": "^2.0.0",
+    "ethers": "^5.7.2",
+    "hardhat": "^2.20.2"
+  }
+}
